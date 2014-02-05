466e62eeee1eade9c74c27c4aa6c2586f72a6f5a
Nalla Kartheek <karthe@codeaurora.org>
2014-02-05 20:53:40 +0530

    framework/base: Do not kill the supplicant on a request to stop it
    Supplicant is killed on obtaining the first TERMINATE event when
    requested to stop from the framework. This shall result in improper
    shutdown of the supplicant without proper cleanup of the resources
    allocated for all the interfaces.
    Moreover, there is no point in killing the supplicant when the
    request itself is to to stop the same. Thus, do not kill the
    supplicant on the TERMINATE event.
    
    Change-Id: I954bb09faa94252eea53a14b090a6cff610c5964
    CRs-Fixed: 601759
    
    Conflicts:
    	wifi/java/android/net/wifi/WifiStateMachine.java

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -2296,11 +2296,13 @@ public class WifiStateMachine extends StateMachine {
         mLastNetworkId = WifiConfiguration.INVALID_NETWORK_ID;
     }
 
-    private void handleSupplicantConnectionLoss() {
+    private void handleSupplicantConnectionLoss(boolean killSupplicant) {
         /* Socket connection can be lost when we do a graceful shutdown
         * or when the driver is hung. Ensure supplicant is stopped here.
         */
-        mWifiMonitor.killSupplicant(mP2pSupported);
+        if (killSupplicant){
+            mWifiMonitor.killSupplicant(mP2pSupported);
+        }
         sendSupplicantConnectionChangedBroadcast(false);
         setWifiState(WIFI_STATE_DISABLED);
     }
@@ -2840,7 +2842,7 @@ public class WifiStateMachine extends StateMachine {
                     break;
                 case WifiMonitor.SUP_DISCONNECTION_EVENT:  /* Supplicant connection lost */
                     loge("Connection lost, restart supplicant");
-                    handleSupplicantConnectionLoss();
+                    handleSupplicantConnectionLoss(true);
                     handleNetworkDisconnect();
                     mSupplicantStateTracker.sendMessage(CMD_RESET_SUPPLICANT_STATE);
                     if (mP2pSupported) {
@@ -2905,13 +2907,13 @@ public class WifiStateMachine extends StateMachine {
                     break;
                 case WifiMonitor.SUP_DISCONNECTION_EVENT:
                     if (DBG) log("Supplicant connection lost");
-                    handleSupplicantConnectionLoss();
+                    handleSupplicantConnectionLoss(false);
                     transitionTo(mInitialState);
                     break;
                 case CMD_STOP_SUPPLICANT_FAILED:
                     if (message.arg1 == mSupplicantStopFailureToken) {
                         loge("Timed out on a supplicant stop, kill and proceed");
-                        handleSupplicantConnectionLoss();
+                        handleSupplicantConnectionLoss(true);
                         transitionTo(mInitialState);
                     }
                     break;
